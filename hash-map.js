5280e18869a21561322b0294f6cfaa5793b63a0b
Sina Bayati <bayati.sina.w@gmail.com>
2024-06-05 16:09:35 +0330

    Implement "set" function

--- hash-map.js
+++ hash-map.js
@@ -1,6 +1,9 @@
 export class HashMap{
   constructor(name){
     this.name = name;
+    this.capacity = 16;
+    this.loadFactor = 0.8;
+    this.buckets = [];
   }
 
   hash(key){
@@ -9,9 +12,44 @@ export class HashMap{
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
-      hashCode = hashCode % 16;
+      hashCode = hashCode % this.capacity;
     }
   
     return hashCode; 
+  }
+
+  set(key,value){
+    const index = this.hash(key);
+    if (index < 0 || index >= this.capacity) {
+      throw new Error("Trying to access index out of bound");
+    }
+    if(this.buckets[index]){
+      let tail = this.buckets[index];
+      while(tail.next !== null){
+        if(tail.key === key){
+          tail.value = value;
+          return false;
+        }
+        tail = tail.next;
+      }
+      if(tail.key === key){
+        tail.value = value;
+        return false;
+      } else {
+        tail.next = new Node(key,value);
+        return true;
+      }
+    } else {  
+      this.buckets[index] = new Node(key,value);
+      return true;
+    }
+  }
+}
+
+class Node{
+  constructor(key,value){
+    this.key = key;
+    this.value = value;
+    this.next = null;
   }
 }
--- index.js
+++ index.js
@@ -2,4 +2,12 @@ import { HashMap } from './hash-map.js';
 
 const H = new HashMap("products");
 console.log(H.name);
-console.log(H.hash("sina"));
+H.set("milk",2);
+H.set("bread",3);
+H.set("chocolate",5);
+H.set("cheese",8);
+console.log(H.set("cheese",10));
+H.set("beef",8);
+H.set("beef",12);
+
+console.log(H.buckets);
